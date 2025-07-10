44e4267981d481b093932b09390a095ba43fd59e
riaatiwarii <riyatrpthi@gmail.com>
2025-07-11 04:53:49 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,10 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+
+
 @dataclass
 class DataIngestionConfig:
   train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -40,6 +44,9 @@ class DataIngestion:
     
 if __name__ == "__main__":
   obj = DataIngestion()
-  obj.initiate_data_ingestion()
+  train_data,test_data=obj.initiate_data_ingestion()
   # print(f"Train data path: {train_data}")
   # print(f"Test data path: {test_data}")
+  
+  data_transformation = DataTransformation()
+  data_transformation.initiate_data_transformation(train_data, test_data)
