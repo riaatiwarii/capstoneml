f242be5c88259bbfd36149f649f0b861fceb3709
riaatiwarii <riyatrpthi@gmail.com>
2025-07-11 05:37:02 +0530

    Trained a Model

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,7 +9,8 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformation
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -49,4 +50,7 @@ if __name__ == "__main__":
   # print(f"Test data path: {test_data}")
   
   data_transformation = DataTransformation()
-  data_transformation.initiate_data_transformation(train_data, test_data)
+  train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data, test_data)
+
+  modeltrainer = ModelTrainer()
+  print(modeltrainer.initiate_model_trainer(train_array=train_arr, test_array=test_arr))
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 from src.exception import CustomException
 
 import dill
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
   try:
@@ -17,3 +18,22 @@ def save_object(file_path, obj):
 
   except Exception as e:
     raise CustomException(e,sys)
+  
+def evaluate_model(x, y, x_test, y_test, models):
+    report = {}
+    try:
+        for i in range(len(list(models))):
+           model = list(models.values())[i]
+           model.fit(x, y)
+           y_train_pred=model.predict(x)
+           y_test_pred=model.predict(x_test)
+
+           train_model_score = r2_score(y, y_train_pred)
+           test_model_score = r2_score(y_test, y_test_pred)
+
+           report[list(models.keys())[i]] = test_model_score
+
+        return report
+           
+    except Exception as e:
+        raise CustomException(e, sys)
